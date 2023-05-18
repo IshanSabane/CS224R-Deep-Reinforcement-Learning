0788d4175fb95e4befa762849617e6456433c399
Ubuntu <ubuntu@ip-172-31-3-94.us-west-2.compute.internal>
2023-05-18 02:34:17 +0000

    Q1.2

--- Homeworks/Homework 3/hw3/q1.sh
+++ Homeworks/Homework 3/hw3/q1.sh
@@ -1,4 +1,11 @@
-python cs224r/scripts/run_iql.py --env_name PointmassEasy-v0 \
+#python cs224r/scripts/run_iql.py --env_name PointmassEasy-v0 \
+#--exp_name iql_tau_0.9_rnd --use_rnd \
+#--num_exploration_steps=20000 \
+#--unsupervised_exploration \
+#--awac_lambda=1 \
+#--iql_expectile=0.9
+
+python cs224r/scripts/run_iql.py --env_name PointmassMedium-v0 \
 --exp_name iql_tau_0.9_rnd --use_rnd \
 --num_exploration_steps=20000 \
 --unsupervised_exploration \
