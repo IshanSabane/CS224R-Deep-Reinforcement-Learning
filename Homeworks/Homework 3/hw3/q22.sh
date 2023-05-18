b4c466c97fc90dbf3f2c0e181e7e28186d4fd393
Ubuntu <ubuntu@ip-172-31-3-94.us-west-2.compute.internal>
2023-05-18 08:59:36 +0000

    q2

--- Homeworks/Homework 3/hw3/q22.sh
+++ Homeworks/Homework 3/hw3/q22.sh
@@ -1,10 +1,17 @@
-#python cs224r/scripts/run_cql.py --env_name PointmassMedium-v0 \
-#--exp_name cql_alpha_0.1_random \
-#--unsupervised_exploration \
-#--offline_exploitation --cql_alpha=0.1
+python cs224r/scripts/run_cql.py --env_name PointmassMedium-v0 \
+--exp_name cql_alpha_0.1_random \
+--unsupervised_exploration \
+--offline_exploitation --cql_alpha=0.1
 
 
 python cs224r/scripts/run_cql.py --env_name PointmassMedium-v0 \
 --exp_name cql_alpha_0.1_rnd --use_rnd \
 --unsupervised_exploration \
 --offline_exploitation --cql_alpha=0.1
+
+
+python cs224r/scripts/run_cql.py --env_name PointmassMedium-v0 \
+--exp_name cql_alpha_0.0_rnd --use_rnd \
+--unsupervised_exploration \
+--offline_exploitation --cql_alpha=0.0
+
